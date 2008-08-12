ffb1c1b4bee147900d3b4dd637cef53684ca9134
liuqing.com <liuqing.com@d2673ad6-3e54-0410-ae54-ff4912804d78>
2008-08-12 08:38:03 +0000

    try distutils.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""模块名
+@version: $Id$
+@author: U{Liu Qing}
+"""
+__author__ =  '刘清'
+__version__=  '1.0'
+__nonsense__ = ''
+from distutils.core import setup
+setup(name='foo',
+      version='1.0',
+      py_modules=['foo'],
+      )
+
