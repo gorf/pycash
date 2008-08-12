9ac8302556b02bcb289951ca6055e9f701e7a9db
liuqing.com <liuqing.com@d2673ad6-3e54-0410-ae54-ff4912804d78>
2008-08-12 08:46:30 +0000

    I use freemind to manage ideas and plans.

---
+++ trunk/pycash.mm
@@ -0,0 +1,7 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1199497721655" ID="Freemind_Link_539380697" MODIFIED="1199497730724" TEXT="pycash">
+<node CREATED="1199497736819" ID="Freemind_Link_1458482332" MODIFIED="1199497740975" POSITION="right" TEXT="pysqlite"/>
+<node CREATED="1199498049298" ID="Freemind_Link_322362281" MODIFIED="1199498052147" POSITION="right" TEXT="pygtk"/>
+</node>
+</map>
