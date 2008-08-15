5f2799d8f162d624608413391d94e347844c15de
liuqing.com <liuqing.com@d2673ad6-3e54-0410-ae54-ff4912804d78>
2008-08-15 17:14:20 +0000

    add some metainfomation.

--- trunk/setup.py
+++ trunk/setup.py
@@ -10,6 +10,11 @@ __nonsense__ = ''
 from distutils.core import setup
 setup(name='pycash',
       version='0.1',
-      py_modules=[''],
+      description='a financial software like gnucash or MS money',
+      author='刘清',
+      author_email='liuqing.com@gmail.com',
+      url='http://code.google.com/p/pycash/',
+      packages=[],
+      py_modules=[],
       )
 
